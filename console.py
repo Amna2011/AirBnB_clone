725f398a71730417b1767ff78affc315229d262a
Amna2011 <amnataha2011@gmail.com>
2024-05-17 19:50:21 -0700

    my 2 commit

--- console.py
+++ console.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-print("(hbnb)")
-"""
-Documented commands (type help <topic>):
-    ========================================
-    EOF  help  quit
-"""
+import cmd
+
+from pexpect import EOF
+class HBNBCommand(cmd.Cmd):
+    x = input("(hbnb)")
+    if x == EOF or x == quit:
+        exit()
+    elif x == help:
+        """Documented commands (type help <topic>):
+========================================
+EOF  help  quit"""
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
