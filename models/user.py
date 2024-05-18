e123706f6ba4b087bdbe607ba800dfa8d0a38923
Amna2011 <amnataha2011@gmail.com>
2024-05-18 05:26:05 -0700

    my 2 commit

--- models/user.py
+++ models/user.py
@@ -2,6 +2,11 @@
 #import models.base_model.BaseModel
 from base_model import BaseModel 
 class User(BaseModel):
+    """my class user """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
     new_user = BaseModel()
     print(new_user.id)
     new_user.save_update()
