0055ef84b4500f0299c58b6506dc1ab584cbe5dc
Amna2011 <amnataha2011@gmail.com>
2024-05-17 20:48:12 -0700

    my 2 commit

---
+++ models/user.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#import models.base_model.BaseModel
+from base_model import BaseModel 
+class User(BaseModel):
+    new_user = BaseModel()
+    print(new_user.id)
+    new_user.save_update()
