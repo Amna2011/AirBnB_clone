c79df19ca4d5bff77d7ee21932063a6b8d88836d
Amna2011 <amnataha2011@gmail.com>
2024-05-18 20:49:09 -0700

    my 2 commit

--- console.py
+++ console.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 import cmd
 
-from pexpect import EOF
+#from pexpect import EOF
+import cmd
+
+class MyConsole(cmd.Cmd):
+    prompt = "(hbnb) "
+    HBNBCommand(cmd.Cmd)
+
 class HBNBCommand(cmd.Cmd):
-    x = input("(hbnb)")
+    x = input()
     if x == EOF or x == quit:
         exit()
     elif x == help:
+        print(
         """Documented commands (type help <topic>):
 ========================================
-EOF  help  quit"""
-
+EOF  help  quit""")
+class MyConsole(cmd.Cmd):
+    prompt = "(hbnb) "
+    HBNBCommand(cmd.Cmd)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
