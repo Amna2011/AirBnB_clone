c3767758b62dcef0d7d25b03651848217052c619
Amna2011 <amnataha2011@gmail.com>
2024-05-19 09:55:33 -0700

    my commit AT

--- models/user.py
+++ models/user.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-from base_model import BaseModel 
+from base_model import BaseModel
+
+
 class User(BaseModel):
     """my class user """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
-    new_user = BaseModel()
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
+    self.new_user = BaseModel()
     print(new_user.id)
     new_user.save_update()
+
 
 if __name__ == '__main__':
     User(BaseModel)
